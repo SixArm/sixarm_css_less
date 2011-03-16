c606c7f8058e94c72baea6fff008852eb439d996
SixArm <sixarm@sixarm.com>
2011-03-16 11:05:31 -0700

    publish

---
+++ less_install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# This installs:
+#  - node.js, from github master to /opt/node
+#  - npm
+#  - less.js
+#
+# The scripts to install node and npm are in the repo sixarm_node_notes
+
+node_ante
+cd /opt
+node_install
+cd -
+
+npm_ante
+npm_install
+
+npm install less
+
